e8ecab9513543c4d3f22f2366bba1b398cbed619
Aleksandar Damnjanovic <aor.aleksandar@gmail.com>
2021-03-03 21:29:16 +0100

    Updated

--- README.md
+++ README.md
@@ -2,5 +2,34 @@ First name of this project was Small Files Remover because I used it for purpose
 
 Purpose: scan through file system for files based on some parameter and execude command to rename, move, remove, edit or simple list selected files. What this app solves? It automatize process of search and execute on huge number of files.
 
-How to compile:
-	g++ main.cpp -o fscalper
+How to compile: g++ main.cpp -o fscalper
+
+
+
+
+
+
+File Scalper is built for routine and fast tasks on large number of files.
+Per every execution it initializes two processes. First process finds all files based on given parameters.
+Second process executes command you have specified, on selected files.
+
+        -h      prints this help.
+        -help   same as -t.
+
+Suport options:
+        -d      include subdirectories in search.
+        -l      list all selected files.
+
+Search options:
+        -sb     files bigger than this value in size expressed in bytes.
+        -ss     files smaller than this value in size expressed in bytes.
+        -se     files with this exact size expressed in bytes.
+        -ex     files with extension expressed without dot.
+        -nc     files with text contained in filename.
+        -nr     files with names matched with regular expression.
+        -Cta    (text files only)files that contain any of provided texts.
+                If you want to search for multiple sentences, separate them with |||
+        -Ctc    (text files only)files that contain all of provided texts.
+                If you want to search for multiple sentences, separate them with |||
+        -Ctr    (text files only)files that contain at least one match by regular expression.
+        -Ctb    (binary files only).files that contain provided hexadecimal pattern.
--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <dirent.h>
 #include <string.h>
 #include <stdlib.h>
-#include <regex.h>
+#include <regex>
 #include <vector>
 #include <iostream>
 #include <cstring>
@@ -50,7 +50,11 @@ int main(int argc, char *argv[]){
             _l = true;
         }
 
-        if (strcmp("-sb", argv[i]) == 0 || strcmp("-ss", argv[i]) == 0 || strcmp("-se", argv[i]) == 0 || strcmp("-ex", argv[i]) == 0){
+        if (strcmp("-sb", argv[i]) == 0 || strcmp("-ss", argv[i]) == 0 
+        || strcmp("-se", argv[i]) == 0 || strcmp("-ex", argv[i]) == 0
+        || strcmp("-nc", argv[i]) == 0 || strcmp("-nr", argv[i]) == 0 
+        || strcmp("-Cta", argv[i]) == 0 || strcmp("-Ctc", argv[i]) == 0
+        || strcmp("-Ctr", argv[i]) == 0 || strcmp("-Ctb", argv[i]) == 0){
             conditions++;
         }
     }
@@ -62,7 +66,8 @@ int main(int argc, char *argv[]){
     int index = 0;
     for (int i = 1; i < argc; i++){
 
-        if (strcmp("-sb", argv[i]) == 0 || strcmp("-ss", argv[i]) == 0 || strcmp("-se", argv[i]) == 0){
+        if (strcmp("-sb", argv[i]) == 0 || strcmp("-ss", argv[i]) == 0 || 
+        strcmp("-se", argv[i]) == 0){
 
             if (strcmp("-sb", argv[i]) == 0)
                 strcpy(con[index].type, "-sb");
@@ -80,10 +85,31 @@ int main(int argc, char *argv[]){
                 con[index].value = value;
                 index++;
             }
-        }else if (strcmp("-ex", argv[i]) == 0){
+        }else if (strcmp("-ex", argv[i]) == 0 || strcmp("-nc", argv[i]) == 0
+        || strcmp("-nr", argv[i]) == 0 || strcmp("-Cta", argv[i]) == 0 
+        || strcmp("-Ctc", argv[i]) == 0 || strcmp("-Ctr", argv[i]) == 0
+        || strcmp("-Ctb", argv[i]) == 0){
 
             if (strcmp("-ex", argv[i]) == 0)
                 strcpy(con[index].type, "-ex");
+
+            if (strcmp("-nc", argv[i]) == 0)
+                strcpy(con[index].type, "-nc");
+
+            if (strcmp("-nr", argv[i]) == 0)
+                strcpy(con[index].type, "-nr");
+
+            if (strcmp("-Cta", argv[i]) == 0)
+                strcpy(con[index].type, "-Cta");
+
+            if (strcmp("-Ctc", argv[i]) == 0)
+                strcpy(con[index].type, "-Ctc");
+
+            if (strcmp("-Ctr", argv[i]) == 0)
+                strcpy(con[index].type, "-Ctr");
+
+            if (strcmp("-Ctb", argv[i]) == 0)
+                strcpy(con[index].type, "-Ctb");
 
             if (argc <= i + 1){
                 printError();
@@ -207,18 +233,36 @@ void printError(){
 void printHelp(){
     printf("\nFile Scalper is built for routine and fast tasks on large number of files.\n");
     printf("Per every execution it initializes two processes. First process finds all files based on given parameters.\n");
-    printf("Second process executes command you have specified on selected files.\n");
+    printf("Second process executes command you have specified, on selected files.\n");
+
     printf("\n\t-h\tprints this help.\n");
-    printf("\t-help\tsame as -t.\n");
+    printf("\t-help\tsame as -t.\n\n");
+
+    printf("Suport options:\n");
     printf("\t-d\tinclude subdirectories in search.\n");
-    printf("\t-l\tlist all selected files.\n");
+    printf("\t-l\tlist all selected files.\n\n");
+
+    printf("Search options:\n");
     printf("\t-sb\tfiles bigger than this value in size expressed in bytes.\n");
     printf("\t-ss\tfiles smaller than this value in size expressed in bytes.\n");
     printf("\t-se\tfiles with this exact size expressed in bytes.\n");
     printf("\t-ex\tfiles with extension expressed without dot.\n");
+    printf("\t-nc\tfiles with text contained in filename.\n");
+    printf("\t-nr\tfiles with names matched with regular expression.\n");
+    printf("\t-Cta\t(text files only)files that contain any of provided texts.\n");
+    printf("\t\tIf you want to search for multiple sentences, separate them with |||\n");
+    printf("\t-Ctc\t(text files only)files that contain all of provided texts.\n");
+    printf("\t\tIf you want to search for multiple sentences, separate them with |||\n");
+    printf("\t-Ctr\t(text files only)files that contain at least one match by regular expression.\n");
+    printf("\t-Ctb\t(binary files only).files that contain provided hexadecimal pattern.\n");
+
+    printf("\n");
 }
 
 bool testFile(char *str){
+
+    if(strcmp(str,"run.bin")==0)
+        return false;
 
     FILE *f;
     f = fopen(str, "r");
@@ -274,6 +318,167 @@ bool testFile(char *str){
                         result = false;
                 }
             }
+
+        if (strcmp("-nc", conn[i].type) == 0)
+            if (result && strlen(str) > 1){
+
+                if (strstr(conn[i].text, " ")){
+
+                    std::vector<std::string> vec;
+
+                    char cc[strlen(conn[i].text)];
+                    strcpy(cc, conn[i].text);
+                    char *ptr = strtok(cc, " ");
+
+                    while (ptr != NULL){
+                        vec.push_back(ptr);
+                        ptr = strtok(NULL, " ");
+                    }
+
+                    bool sub = false;
+                    loop1:for (int i1 = 0; i1 < vec.size(); i1++){
+                        int l = vec[i1].length();
+                        char c[l];
+                        strcpy(c, vec[i1].c_str());
+                        if (strstr(str,c)){
+                            sub = true;
+                            break;
+                        }
+                    }
+
+                    result = sub;
+                }else{
+                    if (!strstr(str,conn[i].text))
+                        result = false;
+                }
+            }
+
+        if (strcmp("-nr", conn[i].type) == 0){
+            
+            std::string value(conn[i].text);
+            std::string search(str);
+            std::regex regex(value);
+            std::smatch sm;
+            std::regex_search(search,sm,regex);
+            if(sm.length()>0)
+                result= true;
+            else
+                result= false;
+            
+        }
+
+        if (strcmp("-Cta", conn[i].type) == 0 || strcmp("-Ctc", conn[i].type) == 0
+        || strcmp("-Ctr", conn[i].type) == 0){
+
+            FILE *f;
+            f=fopen(str,"r");
+            fseek(f,0,SEEK_END);
+            long size=ftell(f);
+            fseek(f,0,SEEK_SET);
+            char buf[size];
+            fread(buf,sizeof(char),size,f);
+            fclose(f);
+
+            if(strcmp("-Ctr", conn[i].type) == 0){
+
+                std::string value(conn[i].text);
+                std::string search(buf);
+                std::regex regex(value);
+                std::smatch sm;
+                std::regex_search(search,sm,regex);
+                if(sm.length()>0)
+                    result= true;
+                else
+                    result= false;
+
+            }else{
+
+                if (strstr(conn[i].text, "|||")){
+
+                    std::vector<std::string> vec;
+
+                    char cc[strlen(conn[i].text)];
+                    strcpy(cc, conn[i].text);
+                    char *ptr = strtok(cc, "|||");
+
+                    while (ptr != NULL){
+                        vec.push_back(ptr);
+                        ptr = strtok(NULL, "|||");
+                    }
+
+                    bool sub = false;
+                    loop2:for (int i1 = 0; i1 < vec.size(); i1++){
+                        int l = vec[i1].length();
+                        char c[l];
+                        strcpy(c, vec[i1].c_str());
+
+                    if(strcmp("-Cta", conn[i].type) == 0){
+                        if (strstr(buf,c)){
+                            sub = true;
+                            break;
+                        } 
+                    }else if(strcmp("-Ctc", conn[i].type) == 0){
+                        if (strstr(buf,c))
+                            sub = true;
+                        else{
+                            sub = false;
+                            break;
+                        }  
+                    }
+                }
+                result = sub;
+
+            }else{
+                if (!strstr(buf,conn[i].text))
+                    result = false;
+            }
+        }
+    }
+    
+        if(strcmp("-Ctb", conn[i].type) == 0){
+
+            result=false;
+            FILE *f;
+            f=fopen(str,"rb");
+            fseek(f,0,SEEK_END);
+            long size=ftell(f);
+            fseek(f,0,SEEK_SET);
+            unsigned char buf[size];
+            fread(buf,sizeof(char),size,f);
+            fclose(f);
+            unsigned char val[sizeof conn[i].text / 2];
+            char *pos=conn[i].text;
+            unsigned char *pos1=buf;
+
+            for(int i=0;i<sizeof conn[i].text / 2;i++){
+                sscanf(pos,"%2hhx", &val[i]);
+                pos+=2;
+            }
+
+            int counter=0;
+            for(int i=0;i<size;i++){
+                float f=(float)i/size;
+                int p=f*100;
+                printf("comparing file %s, %d of %d; %d%\n",str, i, size,p);
+                if(val[counter]==buf[i]){
+                    counter++;
+                    for(int i1=i+1;i1<size;i1++){
+                        if(val[counter]==buf[i1]){
+                            counter++;
+                            if(counter==sizeof val/sizeof(unsigned char)){
+                                result=true;
+                                goto ll;
+                            }
+                        }else{
+                            counter=0;
+                            goto l1;
+                        }
+                    }
+                }
+                l1:system("clear");
+            }
+            ll:;
+        }
     }
 
     return result;
